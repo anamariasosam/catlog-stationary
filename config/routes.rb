0ca669a21abebef1dbad065f3fa9da89f1f1a66f
Ana Maria Sosa <anamariasosam@gmail.com>
2017-04-13 15:06:55 -0500

    add user model and sti with store and customer

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
+  devise_for :users
+  root 'pages#index'
 
   get '*path', to: 'pages#index', constraints: -> (request) do
     !request.xhr? && request.format.html?
   end
 
-  root 'pages#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api, defaults: { format: :json } do
+    scope module: :v1 do
+      resources :sessions, :only => [:create, :destroy]
+      resources :users
+    end
+  end
 end
