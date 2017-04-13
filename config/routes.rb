d13eee3decfd40103d71cbc074c212d7cc63942a
Alex <alexlopezarroyave@gmail.com>
2017-04-13 13:21:21 -0500

    SPA routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  get 'pages/index'
 
+  get '*path', to: 'pages#index', constraints: -> (request) do
+    !request.xhr? && request.format.html?
+  end
+
+  root 'pages#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
