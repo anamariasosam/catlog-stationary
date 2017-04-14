ada1a2788a0f83433dc78af9520cd118d75c615c
Ana Maria Sosa <anamariasosam@gmail.com>
2017-04-14 16:16:39 -0500

    Fix serializers links and add store name to product

--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -1,4 +1,8 @@
 class ProductSerializer < ActiveModel::Serializer
-  attributes :id, :name, :price, :quantity, :image, :description, :photo_id
+  attributes :id, :name, :price, :quantity, :image, :description, :photo_id, :store_name
   has_one :category
+
+  def store_name
+    object.store.name
+  end
 end
--- app/serializers/store_serializer.rb
+++ app/serializers/store_serializer.rb
@@ -1,6 +1,11 @@
 class StoreSerializer < UserSerializer
   attributes :tag_list
 
-  link(:self) { api_store_path(object.id) }
-  link(:products) { api_store_products_path(object.id) }
+  attribute :links do
+    id = object.id
+    {
+      self: api_store_path(id),
+      products: api_store_products_path(id)
+    }
+  end
 end
