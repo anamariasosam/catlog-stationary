7e53511794b632bb2daeb6a4a0a930f64aa3e7ea
Ana Maria Sosa <anamariasosam@gmail.com>
2017-04-14 16:15:56 -0500

    Change ams adapater to json

--- config/initializers/ams.rb
+++ config/initializers/ams.rb
@@ -1,4 +1,4 @@
 require 'active_model_serializers'
 
-ActiveModelSerializers.config.adapter = :json_api
-ActiveModelSerializers.config.key_transform = :underscore
+ActiveModelSerializers.config.adapter = :json
+ActiveModelSerializers.config.key_transform = :camel_lower
